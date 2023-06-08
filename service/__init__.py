99a9241b70943f8a6cce777ae6b358abb26799a6
Zishan <sheikhzishan99@gmail.com>
2023-06-08 08:44:04 -0400

    Added Cors headers

--- service/__init__.py
+++ service/__init__.py
@@ -9,6 +9,7 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
